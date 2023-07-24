b84b55ca794c75bc83c2186760fca23c61c56d6b
Isaac Pontes <isaaclfp@outlook.com>
2023-07-24 20:40:02 -0300

    Cria o controller da rota de reservas

--- src/app.js
+++ src/app.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const ReservationRepository = require("./reservations/ReservationRepository.js");
 const ReservationService = require("./reservations/ReservationService.js");
+const ReservationController = require('./reservations/ReservationController.js');
 
 const app = express();
 
 const reservationRepository = new ReservationRepository();
 const reservationService = new ReservationService(reservationRepository);
+const reservationController = new ReservationController(reservationService);
 
 app.use(express.json());
 
-app.post("/api/reservations", async (req, res) => {
-  try {
-    const { roomId, guestName, checkInDate, checkOutDate } = req.body;
-
-    if (!roomId || !guestName || !checkInDate || !checkOutDate) {
-      return res.status(400).json({ message: "All fields are required." });
-    }
-
-    const reservation = reservationService.createReservation({ roomId, guestName, checkInDate, checkOutDate });
-
-    return res.status(201).json({ message: "Reservation created successfully.", reservation });
-  } catch (error) {
-    return res.status(409).json({ message: error.message });
-  }
-});
+app.post("/api/reservations", reservationController.save);
+app.get("/api/reservations", reservationController.index);
 
 module.exports = app;
---
+++ src/reservations/ReservationController.js
@@ -0,0 +1,32 @@
+class ReservationController {
+  constructor(service) {
+    this.service = service;
+  }
+
+  index = (req, res) => {
+    try {
+      const reservations = reservationService.findAllReservations();
+      return res.json(reservations);
+    } catch (error) {
+      return res.status(409).json({ message: error.message });
+    }
+  }
+
+  save = (req, res) => {
+    try {
+      const { roomId, guestName, checkInDate, checkOutDate } = req.body;
+
+      if (!roomId || !guestName || !checkInDate || !checkOutDate) {
+        return res.status(400).json({ message: "All fields are required." });
+      }
+
+      const reservation = this.service.createReservation({ roomId, guestName, checkInDate, checkOutDate });
+
+      return res.status(201).json({ message: "Reservation created successfully.", reservation });
+    } catch (error) {
+      return res.status(409).json({ message: error.message });
+    }
+  }
+}
+
+module.exports = ReservationController;
